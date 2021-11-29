b8fc7c1e145f0af310237bf68add6ff3bead364e
William Yang <wywilliamyang9@gmail.com>
2021-11-29 17:21:21 -0500

    parent header done

--- parent.h
+++ parent.h
@@ -1,10 +1,19 @@
+#pragma once
+
+_Monitor Printer;
+_Monitor Bank;
+extern MPRNG mprng;
+
 _Task Parent {
-	void main();
   enum States : char {
     Start = 'S',
     Deposit = 'D',
     Finished = 'F'
   };
+  Printer & printer;
+  Bank & bank;
+  unsigned int numStudents, parentalDelay;
+	void main();
   public:
 	Parent( Printer & prt, Bank & bank, unsigned int numStudents, unsigned int parentalDelay );
 };
