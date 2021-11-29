4d97c5b9f971af02f1c231e723630d155c35b559
William Yang <wywilliamyang9@gmail.com>
2021-11-29 17:09:58 -0500

    parent header done

---
+++ parent.cc
@@ -0,0 +1,11 @@
+#include "parent.h"
+#include "printer.h"
+#include "bank.h"
+
+void main(){
+
+}   // Parent::main
+
+Parent( Printer & prt, Bank & bank, unsigned int numStudents, unsigned int parentalDelay ){
+
+}   // Parent::Parent
