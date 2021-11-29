fbcb81345bd766e72df10179230ec7d1695d2bcf
s368yang <s368yang@student.cs.uwaterloo.ca>
2021-11-29 15:25:37 -0500

    courier done

--- watcardoffice.cc
+++ watcardoffice.cc
@@ -2,3 +2,17 @@
 #include "printer.h"
 #include "bank.h"
 
+// _________________________ Courier Task _________________________
+
+void WATCardOffice::Courier::main(){
+
+}   // WATCardOffice::Courier::main
+
+WATCardOffice::Courier::Courier(Printer & printer, Bank & bank, WATCardOffice & office, unsigned int cid):
+	printer(printer), bank(bank), office(office), cid(cid){}	// WATCardOffice::Courier::Courier
+
+WATCardOffice::Courier::~Courier(){
+	printer.print(Printer::Kind::Courier, cid, WATCardOffice::Courier::States::Finished);
+}	// WATCardOffice::Courier::~Courier
+
+// _________________________ WATCardOffice Class _________________________
