bc5897c8a1b84bda4988f5552de224271036e7e5
William Yang <wywilliamyang9@gmail.com>
2021-12-02 14:50:59 -0500

    fixed plant formating

--- bottlingplant.cc
+++ bottlingplant.cc
@@ -4,27 +4,29 @@ BottlingPlant::BottlingPlant( Printer & prt, NameServer & nameServer, unsigned i
     unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour, unsigned int timeBetweenShipments ):
     printer(prt), nameServer(nameServer), truck(Truck(prt, nameServer, *this, numVendingMachines, maxStockPerFlavour)),
     numVendingMachines(numVendingMachines), maxShippedPerFlavour(maxShippedPerFlavour),
-    maxStockPerFlavour(maxStockPerFlavour), timeBetweenShipments(timeBetweenShipments){}
+    maxStockPerFlavour(maxStockPerFlavour), timeBetweenShipments(timeBetweenShipments)
+    {}    // BottlingPlant::BottlingPlant
+
 void BottlingPlant::main(){
     printer.print(Printer::Kind::BottlingPlant, Start);
     for (;!shutdown;) {
         _Accept (~BottlingPlant) {
             shutdown = true;
-        }
-        _Else {
+        } _Else {
             yield(timeBetweenShipments);//pruduction
             printer.print(Printer::Kind::BottlingPlant, Generating, 4*maxShippedPerFlavour);
             productionReady.V();
             truckReady.P();
             for (int i = 0; i < 4; i += 1){
                 truckCargo[i] = maxShippedPerFlavour;
-            }
+            }   // for
             printer.print(Printer::Kind::BottlingPlant, Pickup);
             productionReady.V();
-        }
+        }   // Accept
     }
     printer.print(Printer::Kind::BottlingPlant, Finished);
-}
+}   // BottlingPlant::main
+
 void BottlingPlant::getShipment( unsigned int cargo[] ){
     productionReady.P();
     truckCargo = cargo;
@@ -32,5 +34,5 @@ void BottlingPlant::getShipment( unsigned int cargo[] ){
     productionReady.P();
     if (shutdown) {
         _Throw Shutdown();
-    }
-}
+    }   // if
+}   // BottlingPlant::getShipment
--- bottlingplant.h
+++ bottlingplant.h
@@ -3,24 +3,26 @@
 #include "printer.h"
 #include "truck.h"
 #include <uSemaphore.h>
+
 _Task BottlingPlant {
 	void main();
-  enum States : char {
-    Start = 'S',
-    Generating = 'G',
-    Pickup = 'P',
-    Finished = 'F'
-  };
-  Printer & printer;
-  NameServer & nameServer;
-  Truck truck;
-  bool shutdown = false;
-  uSemaphore truckReady = uSemaphore(0), productionReady = uSemaphore(0);
-  unsigned int numVendingMachines, maxShippedPerFlavour, maxStockPerFlavour, timeBetweenShipments, * truckCargo = nullptr;
-  public:
+	enum States : char {
+		Start = 'S',
+		Generating = 'G',
+		Pickup = 'P',
+		Finished = 'F'
+	};
+	Printer & printer;
+	NameServer & nameServer;
+	Truck truck;
+	bool shutdown = false;
+	uSemaphore truckReady = uSemaphore(0), productionReady = uSemaphore(0);
+	unsigned int numVendingMachines, maxShippedPerFlavour, maxStockPerFlavour;
+	unsigned int timeBetweenShipments, * truckCargo = nullptr;
+	public:
 	_Event Shutdown {};					// shutdown plant
 	BottlingPlant( Printer & prt, NameServer & nameServer, unsigned int numVendingMachines,
-				 unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
-				 unsigned int timeBetweenShipments );
+				unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
+				unsigned int timeBetweenShipments );
 	void getShipment( unsigned int cargo[] );
 };
