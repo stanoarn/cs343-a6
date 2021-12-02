9f83453f617065564790da913e78611e47c1944f
William Yang <wywilliamyang9@gmail.com>
2021-12-02 14:53:56 -0500

    truck format

--- truck.cc
+++ truck.cc
@@ -2,13 +2,15 @@
 
 extern MPRNG mprng;
 
-Truck::Truck( Printer & prt, NameServer & nameServer, BottlingPlant & plant, unsigned int numVendingMachines, unsigned int maxStockPerFlavour ):
-printer(prt), nameServer(nameServer), machines(nameServer.getMachineList()), plant(plant), numVendingMachines(numVendingMachines), maxStockPerFlavour(maxStockPerFlavour)
-{}
+Truck::Truck( Printer & prt, NameServer & nameServer, BottlingPlant & plant, 
+    unsigned int numVendingMachines, unsigned int maxStockPerFlavour ):
+    printer(prt), nameServer(nameServer), machines(nameServer.getMachineList()), 
+    plant(plant), numVendingMachines(numVendingMachines), maxStockPerFlavour(maxStockPerFlavour)
+    {}  // Truck::Truck
 
 void Truck::main(){
     try {
-    printer.print(Printer::Kind::Truck, Start);
+        printer.print(Printer::Kind::Truck, Start);
         for (;;) {
             yield(mprng(10) + 1);
             plant.getShipment(cargo);
@@ -18,24 +20,23 @@ void Truck::main(){
                 VendingMachine * machine = machines[currentMachineIndex];
                 restock(machine);
                 currentMachineIndex = (currentMachineIndex + 1) % numVendingMachines;
-            if (currentMachineIndex == machineIndex) break;
-            }
+                if (currentMachineIndex == machineIndex) break;
+            }   // for
             machineIndex = currentMachineIndex;
-        }
-    }
-    catch(Shutdown &){
+        }   // for
+    } catch(Shutdown &){
         //exit the main loop
         printer.print(Printer::Kind::Truck, Finished);
-    }
-}
+    }   // try
+}   // Truck::main
 
 void Truck::empty(){
     bool result = true;
     for (int i = 0; i < 4; i += 1){
         result = result && cargo[i];
-    }
+    }   // for
     return result;
-}
+}   // Truck::empty
 
 void Truck::restock(VendingMachine * machine){
     printer.print(Printer::Kind::Truck, DeliveryBegin, totalShipment());
@@ -48,23 +49,22 @@ void Truck::restock(VendingMachine * machine){
         notReplenished += spaceLeft - canFill;
         stock[i] = stock[i] + canFill;
         cargo[i] = cargo[i] - canFill;
-    }
+    }   // for
     if (notReplenished > 0){
         printer.print(Printer::Kind::Truck, DeliveryFail, notReplenished);
-    }
-    else {
+    } else {
         printer.print(Printer::Kind::Truck, DeliveryEnd, totalShipment());
-    }
+    }  // if
     if (mprng(100) == 0){
         printer.print(Printer::Kind::Truck, WaitForRepair);
         yield(10);
-    }
-}
+    }   // if
+}   // Truck::restock
 
 unsigned int totalShipment(){
     unsigned int result = 0;
     for (int i = 0; i < 4; i += 1) {
         result += cargo[i];
-    }
+    }   // for
     return result;
-}
+}   // Truck::totalShipment
--- truck.h
+++ truck.h
@@ -4,23 +4,24 @@ _Task BottlingPlant;
 
 _Task Truck {
 	void main();
-  enum States : char {
-    Starting = 'S',
-    Pickup = 'P',
-    DeliveryBegin = 'd',
-    DeliveryFail = 'U',
-    DeliveryEnd = 'D',
-    WaitForRepair = 'X',
-    Finished = 'F'
-  };
+	enum States : char {
+		Starting = 'S',
+		Pickup = 'P',
+		DeliveryBegin = 'd',
+		DeliveryFail = 'U',
+		DeliveryEnd = 'D',
+		WaitForRepair = 'X',
+		Finished = 'F'
+	};
 	Printer & printer;
-  NameServer & nameServer;
-  VendingMachine ** machines;
-  BottlingPlant & plant;
-  unsigned int numVendingMachines, maxStockPerFlavour, cargo[4] = {0,0,0,0}, machineIndex = 0;
-  bool empty();
-  unsigned int totalShipment();
-  void restock(VendingMachine * machine);
-  public:
-  Truck( Printer & prt, NameServer & nameServer, BottlingPlant & plant, unsigned int numVendingMachines, unsigned int maxStockPerFlavour );
+	NameServer & nameServer;
+	VendingMachine ** machines;
+	BottlingPlant & plant;
+	unsigned int numVendingMachines, maxStockPerFlavour, cargo[4] = {0,0,0,0}, machineIndex = 0;
+	bool empty();
+	unsigned int totalShipment();
+	void restock(VendingMachine * machine);
+	public:
+	Truck( Printer & prt, NameServer & nameServer, BottlingPlant & plant, 
+		unsigned int numVendingMachines, unsigned int maxStockPerFlavour );
 };
