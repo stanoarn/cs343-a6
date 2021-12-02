4416aa4a5089ff5b5b17287bb063be8f8ed9f675
William Yang <wywilliamyang9@gmail.com>
2021-12-02 15:30:07 -0500

    nameserver format

--- nameserver.cc
+++ nameserver.cc
@@ -2,8 +2,10 @@
 #include "printer.h"
 
 NameServer::NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents ):
-printer(prt), numVendingMachines(numVendingMachines), numStudents(numStudents), machines(new VendingMachine*[numVendingMachines])
-{}
+    printer(prt), numVendingMachines(numVendingMachines), numStudents(numStudents), 
+    machines(new VendingMachine*[numVendingMachines])
+    {}  // NameServer::NameServer
+
 void NameServer::main(){
     printer.print(Printer::Kind::NameServer, Start);
     for (;machineIndex < numVendingMachines; machineIndex += 1){
@@ -12,34 +14,34 @@ void NameServer::main(){
             machineIndex += 1;
             printer.print(Printer::Kind::NameServer, Register, newMachine->getId());
             bench.signalBlock();
-        }//accept VMregister
-    }//for
+        }   //accept VMregister
+    }   //for
     machineIndex = 0;
     for (;;){
         _Accept(~NameServer){
             bench.signalBlock();
             printer.print(Printer::Kind::NameServer, Finished);
             break;
-        }
-        or _Accept(getMachine) {
+        } or _Accept(getMachine) {
             newMachine = machines[machineIndex];
             printer.print(Printer::Kind::NameServer, NewMachine, studentId, newMachine->getId());
             machineIndex = (machineIndex + 1) % numVendingMachines;
             bench.signalBlock();
-        }
-        or _Accept(getMachineList) { }
-    }
-}
+        } or _Accept(getMachineList) { }    // Accept
+    }   // for
+}   // NameServer::main
 
 void NameServer::VMregister( VendingMachine * vendingmachine ){
     newMachine = vendingmachine;
     bench.wait();
-}
+}   // NameServer::VMregister
+
 VendingMachine * NameServer::getMachine( unsigned int id ){
     studentId = id;
     bench.wait();
     return newMachine;
-}
+}   // NameServer::getMachine
+
 VendingMachine ** NameServer::getMachineList(){
     return machines;
-}
+}   // NameServer::getMachineList
--- nameserver.h
+++ nameserver.h
@@ -1,22 +1,23 @@
 #pragma once
 #include "vendingmachine.h"
+
 _Monitor Printer;
+
 _Task NameServer {
 	void main();
-  enum States : char {
-    Start = 'S',
-    Register = 'R',
-    NewMachine = 'N',
-    Finished = 'F'
-  };
-  Printer & printer;
-  unsigned int numVendingMachines, numStudents,
-                studentId = 0;//communication variable for getMachine
-  VendingMachine ** machines;
-  unsigned int machineIndex = 0;
-  VendingMachine * newMachine;//communication variable for getMachineList
-  uCondition bench;
-  public:
+	enum States : char {
+		Start = 'S',
+		Register = 'R',
+		NewMachine = 'N',
+		Finished = 'F'
+	};
+	Printer & printer;
+	unsigned int numVendingMachines, numStudents, studentId = 0;//communication variable for getMachine
+	VendingMachine ** machines;
+	unsigned int machineIndex = 0;
+	VendingMachine * newMachine;//communication variable for getMachineList
+	uCondition bench;
+	public:
 	NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents );
 	void VMregister( VendingMachine * vendingmachine );
 	VendingMachine * getMachine( unsigned int id );
